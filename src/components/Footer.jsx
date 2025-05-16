aa419abff532dd347cccf3eafba2f45c39d72615
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-05-16 21:33:45 +0530

    footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="shadow-lg">
+      <p className=" max-w-6xl text-center mx-auto px-2 py-5 text-slate-700 font-medium">
+        © 2025 Yummzy. &nbsp;Fresh meals, delivered with love. Fast • Tasty •
+        Reliable ❤️
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
